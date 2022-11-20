62c09db24fbc2cb868f91d09f7491301820a1b9f
Acer <oduvan4ik_888@mail.ru>
2022-11-20 18:46:53 +0300

    Первый

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
 public class Main {
+    Map<String,Integer> map = new HashMap<>();
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    }
+
+    public void addMap(String key, int value){
+       if (!map.containsKey(key)){
+           map.put(key,value);
+       }else {
+           int valueMap = map.get(key);
+           if (valueMap == value){
+               throw new RuntimeException();
+           } else {
+               map.put(key, valueMap + value);
+           }
+
+       }
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Main main = (Main) o;
+        return Objects.equals(map, main.map);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(map);
     }
 }
